6d746e404768be495d8420d45771d047f4851593
Mangrogred <mangrogred@tutamail.com>
2026-03-19 18:25:36 +0200

    Code refactoring && border color

--- config.def.h
+++ config.def.h
@@ -5,23 +5,27 @@
 // Mod5Mask - Shift
 #define MODMASK Mod4Mask
 
-#define BORDER_SIZE 2
-#define BORDER_COLOR 0x333333
+#define BORDER_SIZE 4
+#define BORDER_INACTIVE_COLOR 0x444444
+#define BORDER_ACTIVE_COLOR 0x4444AA
 
-#define WORKSPACES 4 
+#define WORKSPACES 6 
 
 //example:			x("button", MaskForAdditionalButton or 0,		function in c)
-#define TBL(x)  	x("n", 0, 			XCirculateSubwindowsUp(d, r); XSetInputFocus(d, e.xkey.window, 2, 0)) \
-                	x("q", 0, 			XKillClient(d, e.xkey.subwindow)) \
-					x("q", ShiftMask, 	running = false) \
-                	x("e", 0, 			system("xterm &")) \
-                	x("d", 0, 			system("dmenu &")) \
-					x("1", 0, 			switch_ws(d, 0)) \
-					x("2", 0, 			switch_ws(d, 1)) \
-					x("3", 0, 			switch_ws(d, 2)) \
-					x("4", 0, 			switch_ws(d, 3)) \
+#define TBL(x)  	x("q", 0, 			XKillClient(d, e.xkey.subwindow))	\
+					x("q", ShiftMask, 	running = false)					\
+					x("e", 0, 			system("xterm &"))					\
+					x("d", 0, 			system("rofi -show run &")) 		\
+					x("1", 0, 			switch_ws(d, 0))					\
+					x("2", 0, 			switch_ws(d, 1))					\
+					x("3", 0, 			switch_ws(d, 2))					\
+					x("4", 0, 			switch_ws(d, 3))					\
+					x("5", 0, 			switch_ws(d, 4))					\
+					x("6", 0, 			switch_ws(d, 5))					\
 					x("1", ShiftMask, 	move_to_ws(d, e.xkey.subwindow, 0)) \
 					x("2", ShiftMask, 	move_to_ws(d, e.xkey.subwindow, 1)) \
 					x("3", ShiftMask, 	move_to_ws(d, e.xkey.subwindow, 2)) \
-					x("4", ShiftMask, 	move_to_ws(d, e.xkey.subwindow, 3)) 
+					x("4", ShiftMask, 	move_to_ws(d, e.xkey.subwindow, 3)) \
+					x("5", ShiftMask, 	move_to_ws(d, e.xkey.subwindow, 4)) \
+					x("6", ShiftMask, 	move_to_ws(d, e.xkey.subwindow, 5)) \
 
--- main.c
+++ main.c
@@ -12,87 +12,133 @@
 
 #include "config.h"
 
-#define stk(s)      XKeysymToKeycode(d, XStringToKeysym(s))
-#define on(ev, ...)   if (e.type == ev) { __VA_ARGS__; } // if statements works poor with {x}, so {__VA_ARGS__;} is better in this situation (but lsp of nvim is fucking crying abt if() inside on())
-#define keys(k, mod, _)  XGrabKey(d, stk(k), MODMASK | mod, r, 1, 1, 1);
-#define map(k, mod, x)   if (e.xkey.keycode == stk(k) && (e.xkey.state & ~Mod2Mask) == (MODMASK | mod)) { x; }
-#define max(a, b) ((a) > (b) ? (a) : (b))
+#define max(a, b) 		((a) > (b) ? (a) : (b))
+#define stk(s)			XKeysymToKeycode(d, XStringToKeysym(s))
+#define on(ev, x)		if (e.type == ev) { x; } 
+#define keys(k, mod, _)	XGrabKey(d, stk(k), MODMASK | mod, r, 1, 1, 1);
+#define button(b, mod)  XGrabButton(d, b, mod, r, 0, ButtonPressMask | ButtonReleaseMask | PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
+#define map(k, mod, x)	if (e.xkey.keycode == stk(k) && (e.xkey.state & ~Mod2Mask) == (MODMASK | mod)) { x; }
 
 bool running = true;
 Window ws[WORKSPACES][256];
 int ws_count[WORKSPACES] = {0};
 int current_ws = 0;
+Display *d;
+XWindowAttributes attr;
+XButtonEvent start;
+XEvent e;
+Window r;
 
+int xerror(Display *d, XErrorEvent *e);
+void configurerequest(XEvent *e);
+void buttonpress(XEvent *e);
+void motionnotify(XEvent *e);
+void maprequest(XEvent *e);
+void unmapnotify(XEvent *e);
+
+void setfocus(Window w, bool f);
 void switch_ws(Display *d, int new_ws);
 void move_to_ws(Display *d, Window win, int new_ws);
-int xerror(Display *d, XErrorEvent *e);
 
 int main(void)
 {
-	Display *d = XOpenDisplay(0); Window r = DefaultRootWindow(d); XEvent e; XWindowAttributes attr; XButtonEvent start;
-	XSelectInput(d, r, SubstructureRedirectMask | SubstructureNotifyMask);
+	d = XOpenDisplay(0); 
+	r = DefaultRootWindow(d); 
+	XSelectInput(d, r, SubstructureRedirectMask | SubstructureNotifyMask | EnterWindowMask | LeaveWindowMask);
 	TBL(keys);
-	XGrabButton(d, AnyButton, MODMASK, r, 0, ButtonPressMask | ButtonReleaseMask | PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
-	XSetErrorHandler(xerror); // fix crashes
+	button(AnyButton, MODMASK);
+	button(1, 0);
+	XSetErrorHandler(xerror); 
 
 	while (running && !XNextEvent (d, &e)) {
-		on(ConfigureRequest, XMoveResizeWindow(d, e.xconfigurerequest.window, 0, 0, e.xconfigurerequest.width, e.xconfigurerequest.height))
-		on(ButtonPress,
-			if (e.xbutton.subwindow != None) { 
-				XRaiseWindow(d, e.xbutton.subwindow);
-				XSetInputFocus(d, e.xbutton.subwindow, RevertToParent, CurrentTime);
-				XGetWindowAttributes(d, e.xbutton.subwindow, &attr);
-				if (!attr.override_redirect) 
-					start = e.xbutton;
-			}
-			XAllowEvents(d, ReplayPointer, CurrentTime);
-		)
-		on(MotionNotify,
-			if (start.subwindow != None) {
-				int xdiff = e.xbutton.x_root - start.x_root;
-				int ydiff = e.xbutton.y_root - start.y_root;
-				XMoveResizeWindow(d, start.subwindow,
-					attr.x + (start.button==1 ? xdiff : 0),
-					attr.y + (start.button==1 ? ydiff : 0),
-					max(1, attr.width + (start.button==3 ? xdiff : 0)),
-					max(1, attr.height + (start.button==3 ? ydiff : 0)));
-			}
-		)
-        on(MapRequest, 
-				XGetWindowAttributes(d, e.xmaprequest.window, &attr);
-				if (!attr.override_redirect) {
-					bool found = false;
-					for (int w = 0; w < WORKSPACES; w++)
-						for (int i = 0; i < ws_count[w]; i++)
-							if (ws[w][i] == e.xmaprequest.window) found = true;
-					if (!found)
-						ws[current_ws][ws_count[current_ws]++] = e.xmaprequest.window;
-					XSelectInput(d, e.xmaprequest.window, EnterWindowMask);
-					XSetWindowBorderWidth(d, e.xmaprequest.window, BORDER_SIZE);
-					XSetWindowBorder(d, e.xmaprequest.window, BORDER_COLOR);
-				}
-				XMapWindow(d, e.xmaprequest.window);
-		)
-		on(UnmapNotify,
-			if (e.xunmap.send_event) {
-				for (int w = 0; w < WORKSPACES; w++) {
-					for (int i = 0; i < ws_count[w]; i++) {
-						if (ws[w][i] == e.xunmap.window) {
-							ws[w][i] = ws[w][--ws_count[w]];
-							break;
-						}
-					}
-				}
-			}
-		)
-		on(ButtonRelease, start.subwindow = None)
-		on(KeyPress, TBL(map))
+		on(ConfigureRequest,	configurerequest(&e))
+		on(ButtonPress,			buttonpress(&e))
+		on(EnterNotify,			setfocus(e.xcrossing.window, 1))
+		on(LeaveNotify,			setfocus(e.xcrossing.window, 0))
+		on(MotionNotify,		motionnotify(&e))
+        on(MapRequest,			maprequest(&e))
+		on(UnmapNotify,			unmapnotify(&e))
+		on(ButtonRelease,		start.subwindow = None)
+		on(KeyPress,			TBL(map))
     }
 	return 0;
 }
 
+void configurerequest(XEvent *e) {
+	XConfigureRequestEvent *ev = &e->xconfigurerequest;
+	XMoveResizeWindow(d, ev->window, 0, 0, ev->width, ev->height);
+}
+
+void buttonpress(XEvent *e) {
+	XButtonPressedEvent *ev = &e->xbutton;
+	setfocus(ev->subwindow, 1);
+	if (ev->subwindow != None && ev->state & MODMASK) { 
+		XRaiseWindow(d, ev->subwindow);
+		XSetInputFocus(d, ev->subwindow, RevertToParent, CurrentTime);
+		XGetWindowAttributes(d, ev->subwindow, &attr);
+		if (!attr.override_redirect) 
+			start = *ev;
+	}
+	XAllowEvents(d, ReplayPointer, CurrentTime);
+}
+
+void motionnotify(XEvent *e) {
+	XButtonPressedEvent *ev = &e->xbutton;
+	if (start.subwindow != None) {
+		int xdiff = ev->x_root - start.x_root;
+		int ydiff = ev->y_root - start.y_root;
+		XMoveResizeWindow(d, start.subwindow,
+			attr.x + (start.button==1 ? xdiff : 0),
+			attr.y + (start.button==1 ? ydiff : 0),
+			max(1, attr.width + (start.button==3 ? xdiff : 0)),
+			max(1, attr.height + (start.button==3 ? ydiff : 0)));
+	}
+}
+
+void maprequest(XEvent *e) {
+	XMapRequestEvent *ev = &e->xmaprequest;
+	XGetWindowAttributes(d, ev->window, &attr);
+	if (!attr.override_redirect) {
+		bool found = false;
+		for (int w = 0; w < WORKSPACES; w++)
+			for (int i = 0; i < ws_count[w]; i++)
+				if (ws[w][i] == ev->window) found = true;
+		if (!found)
+			ws[current_ws][ws_count[current_ws]++] = ev->window;
+		XSelectInput(d, ev->window, EnterWindowMask | LeaveWindowMask);
+		XSetWindowBorderWidth(d, ev->window, BORDER_SIZE);
+		XSetWindowBorder(d, ev->window, BORDER_INACTIVE_COLOR);
+	}
+	XMapWindow(d, ev->window);
+}
+
+void unmapnotify(XEvent *e) {
+	XUnmapEvent *ev = &e->xunmap;
+	if (ev->send_event) {
+		for (int w = 0; w < WORKSPACES; w++) {
+			for (int i = 0; i < ws_count[w]; i++) {
+				if (ws[w][i] == ev->window) {
+					ws[w][i] = ws[w][--ws_count[w]];
+					break;
+				}
+			}
+		}
+	}
+}
+
 int xerror(Display *d, XErrorEvent *e) {
 	return 0; 
+}
+
+void setfocus(Window w, bool f) {
+	if (w == None || w == r) return;
+	if (f) {
+		XSetInputFocus(d, w, RevertToParent, CurrentTime);
+		XSetWindowBorder(d, w, BORDER_ACTIVE_COLOR);
+	} else {
+		XSetInputFocus(d, r, RevertToPointerRoot, CurrentTime);
+		XSetWindowBorder(d, w, BORDER_INACTIVE_COLOR);
+	}
 }
 
 void switch_ws(Display *d, int new_ws) {
@@ -117,9 +163,8 @@ void move_to_ws(Display *d, Window win, int new_ws) {
 
 
 /* TODO:
- * 1. add spawn function and maybe more like in dwm
- * 2. add tiling
- * 3. add status bar
- * 4. add logs to xerror
- * 5. cheese
+ * 1. add tiling
+ * 2. add status bar
+ * 3. add logs to xerror
+ * 4. cheese
 */
